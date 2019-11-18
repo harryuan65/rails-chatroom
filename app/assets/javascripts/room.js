6345656987d789e51ad46d5f8f6a19dfead5cb66
Harry Yuan <harryuan.65@gmail.com>
2019-11-18 17:42:49 +0800

    changed to broadcast with actioncable w\redis

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -1,40 +1,66 @@
-$(function() {
-  $('#form-send-msg').on('submit',function(e){
-    e.preventDefault();
-    var formData = $(this).serializeArray();
-    var data = {
-      room_message:{}
-    }
-    formData.forEach(e=>{
-       if(e.name.includes("room_message")){
-         data.room_message[`${e.name.replace(/room_message\[|\]/g,"")}`] = e.value
-       }
-       else{
-         data[e.name] = e.value;
-       }
-    })
-    console.log(data);
-    $.ajax({
-      url:$(this).attr("action"),
-      type:"POST",
-      contentType:"application/json",
-      processData: false,
-      data:JSON.stringify(data)
-    })
-    .done(data=>{
-      $('#input-msg').val("");
-      $.get(window.location.href,function(resText){
-        var find = $('#chat',resText);
-        if(find.length){
-          console.log("Updated")
-          $('#chat').replaceWith(find);
-        }
-      })
-    })
+// $(function() {
+//   scrolltoBottom();
+//   $('#form-send-msg').on('submit',function(e){
+//     e.preventDefault();
+//     var currentScrollTop = $("#chat").scrollTop();
+//     var formData = $(this).serializeArray();
+//     var data = {
+//       room_message:{}
+//     }
+//     formData.forEach(e=>{
+//        if(e.name.includes("room_message")){
+//          data.room_message[`${e.name.replace(/room_message\[|\]/g,"")}`] = e.value
+//        }
+//        else{
+//          data[e.name] = e.value;
+//        }
+//     })
+//     console.log(data);
+//     $.ajax({
+//       url:$(this).attr("action"),
+//       type:"POST",
+//       contentType:"application/json",
+//       processData: false,
+//       data:JSON.stringify(data)
+//     })
+//     .done(data=>{
+//       $('#input-msg').val("");
+//       $.get(window.location.href,function(resText){
+//         var find = $('.room-msg-row[id^=msg-]',resText).last();
+//         if(find.length){
+//           console.log("Updated")
+//           $('#chat').append(find);
+//           console.log(`Current: ${$("#chat").scrollTop()}`)
+//           UpdateScroll(currentScrollTop);
+//         }
+//         else{
+//           console.log("No updates")
+//         }
+//       })
+//     })
 
-  });
+//   });
 
-  $('#new_room_message').on('ajax:success', function(a, b,c ) {
-    $(this).find('input[type="text"]').val('');
-  });
-});
+//   $('#new_room_message').on('ajax:success', function(a, b,c ) {
+//     $(this).find('input[type="text"]').val('');
+//   });
+// });
+// function scrolltoBottom(){
+//   var totalScrollHeight = $("#chat")[0].scrollHeight;
+//   var divHeight = $("#chat").height();
+//   var toHeight = totalScrollHeight - divHeight;
+//   // console.log(`${totalScrollHeight} - ${divHeight} = ${totalScrollHeight-divHeight}`)
+//   // console.log(`set to: ${toHeight}`)
+//   $("#chat").animate({
+//     scrollTop: `${toHeight}px`
+//   },function(){
+//     // console.log("scrolled");
+//   })
+// }
+// function UpdateScroll(currentScrollTop=0){
+//   $("#chat").animate({
+//     scrollTop: `${currentScrollTop+80}px`
+//   },function(){
+//     // console.log("scrolled");
+//   })
+// }
--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -4,6 +4,11 @@ class RoomMessagesController < ApplicationController
     @room_message = RoomMessage.create user: current_user,
                                        room: @room,
                                        message: params.dig(:room_message, :message)
+    broadcast_msg = @room_message.attributes
+    broadcast_msg["user_avatar_url"] = current_user.gravatar_url
+    broadcast_msg["username"] = current_user.username
+    RoomChannel.broadcast_to @room,broadcast_msg #要用Redis+ActionCable就多這行
+    # RoomChannel.broadcast_to @room,@room_message #要用Redis+ActionCable就多這行
   end
 
   protected
